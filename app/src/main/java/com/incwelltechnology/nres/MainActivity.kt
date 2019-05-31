a53bfaa4c9fa0200474086667ede5b6bc49c67cf
iamsannyrai <iam.sannyrai@gmail.com>
2019-06-01 01:15:48 +0545

    commit

--- app/src/main/java/com/incwelltechnology/nres/MainActivity.kt
+++ app/src/main/java/com/incwelltechnology/nres/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.incwelltechnology.nres
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
+
